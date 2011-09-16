f459511404be5386004ec2c06a5d2776c754ab70
xuwei_k <6b656e6a69@gmail.com>
2011-09-17 00:25:12 +0900

    format

--- src/main/scala/com/github/xuwei_k/MyMailService.scala
+++ src/main/scala/com/github/xuwei_k/MyMailService.scala
@@ -10,61 +10,56 @@ import javax.servlet.http._
 import javax.mail.{Session}
 import javax.mail.internet.MimeMessage
 import com.google.appengine.api.mail.{MailService,MailServiceFactory}
-	
-
 
 object MyMailService {
-	
-	//添付ファイル
-	case class Attachment(name:String,data:InputStream)
-	
-	/** メール送信
-	 *
-	 */
-	def send(sendAddress:String,attachments:MailService.Attachment *){
-	
-	  //なにかしら、本文に文字を入れないと、送信できないらしい!!!
-	  val message = new MailService.Message( "test1@xuwei-k.appspotmail.com" , sendAddress ,"test" , "body" )
+  //添付ファイル
+  case class Attachment(name:String,data:InputStream)
+
+  /** メール送信
+   *
+   */
+  def send(sendAddress:String,attachments:MailService.Attachment *){
+
+    //なにかしら、本文に文字を入れないと、送信できないらしい!!!
+    val message = new MailService.Message( "test1@xuwei-k.appspotmail.com" , sendAddress ,"test" , "body" )
       message.setAttachments(attachments:_*)
       MailServiceFactory.getMailService.send(message)
-	}
-	
-    /**
-     * メール解析
-     *
-     * @param part
-     */
-    def partAnalysis(part:Part,list:List[Attachment]):List[Attachment]= {
+  }
 
-	  if (part.isMimeType("multipart/*")) {
-		  
-		val content = part.getContent.asInstanceOf[Multipart]
-		
-	    // 含まれるパートを再帰的に処理
-		list ++ {
-		  (0 until content.getCount).map{
-		    n => partAnalysis(content.getBodyPart(n),Nil) 
-		  }.flatten
-		}
-	  } else if (part.isMimeType("text/plain")) {
-		  // テキストの場合なにもしない
-	    list
-	  } else {
-		// その他の場合
-		val disp = part.getDisposition
-		
-		if (disp == null || disp.equalsIgnoreCase(Part.ATTACHMENT)) {
-		  getAttachments(part) :: list
-		}else{
-		  list
-		}
+  /**
+   * メール解析
+   */
+  def partAnalysis(part:Part,list:List[Attachment]):List[Attachment]= {
+
+    if (part.isMimeType("multipart/*")) {
+
+      val content = part.getContent.asInstanceOf[Multipart]
+
+      // 含まれるパートを再帰的に処理
+      list ++ {
+        (0 until content.getCount).map{
+          n => partAnalysis(content.getBodyPart(n),Nil) 
+        }.flatten
+      }
+    } else if (part.isMimeType("text/plain")) {
+      // テキストの場合なにもしない
+      list
+    } else {
+      // その他の場合
+      val disp = part.getDisposition
+
+      if (disp == null || disp.equalsIgnoreCase(Part.ATTACHMENT)) {
+        getAttachments(part) :: list
+      }else{
+        list
       }
     }
+  }
 
-    /** 添付ファイル取得
-     */
-    private def getAttachments(part:Part):Attachment = {
-      var name = MimeUtility.decodeText(part.getFileName())
-      Attachment( name , part.getInputStream )
-    }
+  /** 添付ファイル取得
+   */
+  private def getAttachments(part:Part):Attachment = {
+    var name = MimeUtility.decodeText(part.getFileName())
+    Attachment( name , part.getInputStream )
+  }
 }
