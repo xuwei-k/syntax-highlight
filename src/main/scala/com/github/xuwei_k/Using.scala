2017a9e933890b43a740dc6ea69582eff2f7b83c
xuwei-k <6b656e6a69@gmail.com>
2012-01-31 04:27:33 +0900

    add comment

--- src/main/scala/com/github/xuwei_k/Using.scala
+++ src/main/scala/com/github/xuwei_k/Using.scala
@@ -1,5 +1,7 @@
 package com.github.xuwei_k
 
+// scalatra にあるものか、scala arm 使うべきか
+// https://github.com/scalatra/scalatra/blob/2.0.3/core/src/main/scala/org/scalatra/util/package.scala
 object Using {
   def using[A, B](resource: A)(func: A => B)(implicit closer: Closer[A]):B =
     try {
@@ -14,12 +16,12 @@ object Using {
 
   object Closer {
     def apply[A](f: A => Unit) = new Closer[A] {
-      def apply(resource: A) = f(resource) 
+      def apply(resource: A) = f(resource)
     }
   }
- 
+
   implicit val IoSourceCloser = Closer[io.Source](_.close)
 //  implicit val disposer  = Closer[{def dispose}](_.dispose)
   implicit val closer = Closer[{def close()}](_.close)
 }
- 
+
