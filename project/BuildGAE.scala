4b2c2d731094424fc43e2b0b60b28a5c57938643
xuwei-k <6b656e6a69@gmail.com>
2012-12-24 06:32:05 +0900

    libraries version

--- project/BuildGAE.scala
+++ project/BuildGAE.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object BuildGAE extends Build{
 
-  val gaeSDK = "1.5.2"
+  val gaeSDK = "1.7.3"
 
   lazy val root = Project("syntax-highlight", file("."),
     settings = {
@@ -12,7 +12,7 @@ object BuildGAE extends Build{
       Seq(
         scalaVersion := "2.9.2",
         libraryDependencies ++= Seq(
-           "org.eclipse.jetty" % "jetty-webapp" % "7.4.5.v20110725" % "container" // 7.4.5.v20110725 fail download :(
+           "org.eclipse.jetty" % "jetty-webapp" % "7.6.8.v20121106" % "container"
           ,"javax.servlet" % "servlet-api" % "2.5" % "provided"
           ,"commons-fileupload" % "commons-fileupload" % "1.2.2"
           ,"com.google.appengine" % "appengine-java-sdk" % gaeSDK
@@ -20,7 +20,7 @@ object BuildGAE extends Build{
           ,"net.kindleit" % "gae-runtime" % gaeSDK
           ,"org.scalatra" % "scalatra_2.9.1" % "2.0.4"
           ,"net.databinder" % "pamflet-knockoff_2.9.1" % "0.4.1"
-          ,"org.scalaz" %% "scalaz-core" % "7.0.0-M4"
+          ,"org.scalaz" %% "scalaz-core" % "7.0.0-M7"
           ,"com.jsuereth" %% "scala-arm" % "1.2"
         )
       )
