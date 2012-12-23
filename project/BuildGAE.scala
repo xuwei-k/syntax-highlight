c3438cec6b659a57c4e8d712cb6a842bf4fad04a
xuwei-k <6b656e6a69@gmail.com>
2012-12-24 06:19:19 +0900

    delete sxr

--- project/BuildGAE.scala
+++ project/BuildGAE.scala
@@ -5,9 +5,6 @@ object BuildGAE extends Build{
 
   val gaeSDK = "1.5.2"
 
-  val sourceCount    = TaskKey[Unit]("source-count")
-  val createSxrSlide = TaskKey[Unit]("create-sxr-slide")
-
   lazy val root = Project("syntax-highlight", file("."),
     settings = {
       Defaults.defaultSettings ++
@@ -26,36 +23,8 @@ object BuildGAE extends Build{
           ,"org.scalaz" %% "scalaz-core" % "7.0.0-M4"
           ,"com.jsuereth" %% "scala-arm" % "1.2"
         )
-        ,resolvers ++= Seq(
-            "Sonatype Nexus Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
-           ,"xuwei-k repo" at "http://xuwei-k.github.com/mvn"
-         )
-        ,addCompilerPlugin("org.scala-tools.sxr" %% "sxr" % "0.2.8-SNAPSHOT")
-        ,scalacOptions <+= scalaSource in Compile map { "-P:sxr:base-directory:" + _.getAbsolutePath }
-        ,sourceCount <<= ( sources in Compile , sources in Test ) map{ (main,test) =>
-           println{
-             "\nmain " + main.map{f => IO.readLines(f).size}.sum +
-             "\ntest " + test.map{f => IO.readLines(f).size}.sum
-           }
-        }
-        ,createSxrSlide <<= ( sources in Compile , sources in Test ) map{ (main,test) =>
-          Seq(main -> "main" ,test -> "test" ).foreach{ case (files,n) =>
-            IO.write( file("slide") / n , create(SxrBaseURL + n + "/",files.map{_.getName}) )
-          }
-        }
       )
     }
   )
 
-  val SxrBaseURL = "http://xuwei-k.github.com/syntax-highlight/"
-
-  def dom(src:String,w:Int,h:Int):xml.Elem = {
-    <iframe src={src} width={w.toString} height={h.toString} />
-  }
-
-  def create(baseURL:String,files:Seq[String],w:Int = 1200,h:Int = 600):String = {
-    files.map{ f =>
-      "\n\n!SLIDE\n\n" + dom( baseURL + f , w , h )
-    }.mkString
-  }
 }
