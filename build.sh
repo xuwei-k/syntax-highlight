a8248ab5e57c011ba0c1298d9b572fbfa52b9328
xuwei-k <6b656e6a69@gmail.com>
2013-06-14 11:40:09 +0900

    fix build.sh. update sdk version

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-SDK=appengine-java-sdk-1.7.6
+SDK=appengine-java-sdk-1.8.1
 SDK_ZIP=$SDK.zip
 wget http://googleappengine.googlecode.com/files/$SDK_ZIP &&
 unzip $SDK_ZIP &&
@@ -7,4 +7,4 @@ unset APPENGINE_SDK_HOME &&
 export APPENGINE_SDK_HOME=./$SDK &&
 wget https://raw.github.com/paulp/sbt-extras/master/sbt &&
 chmod u+x ./sbt &&
-./sbt -debug -mem 512 test
+./sbt -debug test
