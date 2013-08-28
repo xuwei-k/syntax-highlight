ccc9299c38ab3be7678c84cf1bb545976de11f30
xuwei-k <6b656e6a69@gmail.com>
2013-08-28 22:43:39 +0900

    update sbt 0.13.0, scala 2.9.3 etc

--- project/BuildGAE.scala
+++ project/BuildGAE.scala
@@ -3,14 +3,14 @@ import Keys._
 
 object BuildGAE extends Build{
 
-  val gaeSDK = "1.7.5"
+  val gaeSDK = "1.8.3"
 
   lazy val root = Project("syntax-highlight", file("."),
     settings = {
       Defaults.defaultSettings ++
       sbtappengine.Plugin.appengineSettings ++
       Seq(
-        scalaVersion := "2.9.2",
+        scalaVersion := "2.9.3",
         libraryDependencies ++= Seq(
            "org.eclipse.jetty" % "jetty-webapp" % "7.6.8.v20121106" % "container"
           ,"javax.servlet" % "servlet-api" % "2.5" % "provided"
@@ -20,8 +20,8 @@ object BuildGAE extends Build{
 //          ,"net.kindleit" % "gae-runtime" % gaeSDK
           ,"org.scalatra" % "scalatra_2.9.1" % "2.0.4"
           ,"net.databinder" % "pamflet-knockoff_2.9.1" % "0.4.1"
-          ,"org.scalaz" %% "scalaz-core" % "7.0.0-RC1"
-          ,"com.jsuereth" %% "scala-arm" % "1.3"
+          ,"org.scalaz" %% "scalaz-core" % "7.1.0-M2"
+          ,"com.jsuereth" % "scala-arm_2.9.2" % "1.3"
         )
       )
     }
--- project/project/plugins.scala
+++
@@ -1,8 +0,0 @@
-import sbt._
-
-object PluginDef extends Build {
-  lazy val root = Project("plugins", file(".")) dependsOn(
-    uri("git://github.com/sbt/sbt-appengine#0.4.1")
-  )
-}
-
